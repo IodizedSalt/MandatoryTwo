3e0d24ebd2156edb7fddac640575a7b821dc8552
IodizedSalt <christophermoeller2000@gmail.com>
2017-11-14 14:51:16 +0100

    First commit

---
+++ src/main/java/com/mandatory/MandatorytwoApplication.java
@@ -0,0 +1,12 @@
+package com.mandatory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandatorytwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandatorytwoApplication.class, args);
+	}
+}
