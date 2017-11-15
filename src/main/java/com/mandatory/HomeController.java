b7a69d2f23a50d1f14c3966abf92d2b5cf887621
IodizedSalt <christophermoeller2000@gmail.com>
2017-11-15 16:25:21 +0100

    Implemented user type selection and their respective templates

--- src/main/java/com/mandatory/HomeController.java
+++ src/main/java/com/mandatory/HomeController.java
@@ -1,4 +1,16 @@
 package com.mandatory;
-// This is a test commit
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String homePage(){
+        return "homepage";
+    }
+    @GetMapping("/student")
+    public String studentPage(){
+        return "student";
+    }
 }
