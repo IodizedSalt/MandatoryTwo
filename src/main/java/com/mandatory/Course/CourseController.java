cb62c5e12735fe44fc07d2b410d70900a842e9ee
gab <mannu.gab@gmail.com>
2017-11-23 13:28:50 +0100

    connection with homepage

--- src/main/java/com/mandatory/Course/CourseController.java
+++ src/main/java/com/mandatory/Course/CourseController.java
@@ -27,21 +27,15 @@ public class CourseController {
         return courseService.getAllCourses();
     }
 
-
     @RequestMapping("/courses/{id}")
     public Course getCourse(@PathVariable Integer id){
         return courseService.getCourse(id);
     }
 
-
-
-
     @RequestMapping(method = RequestMethod.POST, value ="/courses",consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addCourse(@RequestBody Course course){
         courseService.addCourse(course);
     }
-
-
 
     @GetMapping("/course/show")
     public ModelAndView show(){
@@ -51,52 +45,17 @@ public class CourseController {
         return mv;
     }
 
-    //DOESN'T WORK
-//    @RequestMapping(value = "/course/show", method = RequestMethod.GET)
-//    public ModelAndView show(
-//        @RequestParam(name = "id",defaultValue = "NO_ID")
-//        String id,
-//        @RequestParam(name = "namee", defaultValue = "NO_NAMED")
-//        String namee,
-//        @RequestParam(name = "named", defaultValue = "NO_NAMED")
-//        String named){
-//
-//
-//
-//        Course course = new Course();
-//        course.setNamee(namee);
-//        course.setNamed(named);
-//        cr.save(course);
-//
-//        ModelAndView mv = new ModelAndView("course");
-//        mv.getModel().put("usersList", cr.findAll());
-//        mv.getModel().put("Couse", course);
-//
-//        return mv;
-//
-//    }
-
-
     @RequestMapping(method = RequestMethod.DELETE, value ="courses/{id}")
     public void deleteCourse(@PathVariable Integer id){
         courseService.deleteCourse(id);
     }
-//    @RequestMapping(method = RequestMethod.PUT, value ="/topics/{topicId}/courses/{id}")
-//    public void updateCourse(@RequestBody Course course, @PathVariable String id, @PathVariable String topicId){
-//        course.setTopic(new Topic(topicId, "",""));
-//        courseService.updateCours(course);
+
+
+//
+//    @RequestMapping(method = RequestMethod.PUT, value ="courses/{id}")
+//    public void updateCourse(@RequestBody Course course, @PathVariable String id){
+//        courseService.updateCourse(course);
 //    }
-
-
-
-
-//TO DO
-//    @RequestMapping(method = RequestMethod.PUT, value ="/topics/{topicId}/courses/{id}")
-//    public void updateCourse(@RequestBody Course course, @PathVariable String id, @PathVariable String topicId){
-//        course.setTopic(new Topic(topicId, "",""));
-//        courseService.updateCours(course);
-//    }
-
 
 
 
